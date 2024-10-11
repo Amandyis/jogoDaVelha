155c561c18cea1b0e90c6cacfa52e70e4a8232d2
Amanda Ribeiro <amandavictoria11@outlook.com>
2024-10-11 16:18:21 -0300

    Consigo escrever quando clico no botão

--- js/jogar.js
+++ js/jogar.js
@@ -3,37 +3,51 @@ const indicaNome = document.getElementById('botaoEFrase');
 let ganhou = false;
 let empatou = false;
 
+
 let dados = sessionStorage.getItem("jogadores");
 let jogadores = JSON.parse(dados);
 
+const jogador1 = jogadores.jogador1;
+const jogador2 = jogadores.jogador2;
 
+let jogada = 0
 
 export function jogar() {
 
-    let dados = sessionStorage.getItem("jogadores");
-    let jogadores = JSON.parse(dados);
+        let dados = sessionStorage.getItem("jogadores");
+        let jogadores = JSON.parse(dados);
 
-    const jogador1 = jogadores.jogador1;
-    const jogador2 = jogadores.jogador2;
+        const jogador1 = jogadores.jogador1;
+        const jogador2 = jogadores.jogador2;
 
-    let jogadorInicial = '';
+        let jogadorInicial = '';
 
-    if (escolhAleatoria() == 1) {
-        jogadorInicial = jogador1
-    } else {
-        jogadorInicial = jogador2
-    }
+        if (escolhAleatoria() == 1) {
+            jogadorInicial = jogador1
+        } else {
+            jogadorInicial = jogador2
+        }
 
-    indicaNome.innerHTML = `<p class="frase">Vez do jogador: ${jogadorInicial.nome}</p>`
+        indicaNome.innerHTML = `<p class="frase">Vez do jogador: ${jogadorInicial.nome}</p>`
 
-    campo.forEach(function (elemento) {
-        elemento.addEventListener('click', function (event) {
-            event.preventDefault();
+        console.log(campo)
 
-            console.log('oi')
 
+        campo.forEach((item) => {
+            item.innerHTML = "";
+            item.addEventListener("click", function () {
+                while(jogada == 0){
+                    item.innerHTML = jogadorInicial.simbolo
+                    jogada += 1
+                }
+                let proximo = (jogadorInicial.simbolo == 'X') ? 'O' : 'X';
+                item.innerHTML = proximo
+                
+            })
         });
-    });
+
+
+
 }
 
 
@@ -43,18 +57,3 @@ function escolhAleatoria() {
     return opcoes.charAt(Math.floor(Math.random() * tamanho));
 }
 
-
-
-
-
-// while (ganhou !== true || empatou !== true) {
-
-//     let jogada = 0
-//     if (jogada === 0) {
-//         escolhAleatoria()
-//         jogar()
-//         jogada +=1
-//     }else{
-//         jogar()
-//     }
-// }
